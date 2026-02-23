aa841aaf33a98ea6be8f6d5690f6068d66301d83
maulanazh15 <maulanazh15@gmail.com>
2026-02-23 08:28:25 +0700

    Update monitor speed to 115200 and refactor debug logging mechanism

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,10 @@
 
 #include <RtcDS1302.h>
 
-// ==== DEBUG FLAG ====
-#define DEBUG 1
+// ==== PENGATURAN LOG/DEBUG ====
+// Ubah menjadi 'false' jika Anda sudah tidak ingin melihat log di Serial Monitor
+const bool DEBUG_MODE = false; 
+const bool FIX_TIME = false;
 
 // Pin RTC
 ThreeWire myWire(4, 5, 2); // IO, SCLK, CE
@@ -22,29 +24,29 @@ RtcDS1302<ThreeWire> Rtc(myWire);
 
 void printDateTime(const RtcDateTime &dt)
 {
-#if DEBUG
-    char datestring[26];
-    snprintf_P(datestring,
-               countof(datestring),
-               PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
-               dt.Month(),
-               dt.Day(),
-               dt.Year(),
-               dt.Hour(),
-               dt.Minute(),
-               dt.Second());
-    Serial.print(datestring);
-#endif
+    if (DEBUG_MODE) {
+        char datestring[26];
+        snprintf_P(datestring,
+                   countof(datestring),
+                   PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
+                   dt.Month(),
+                   dt.Day(),
+                   dt.Year(),
+                   dt.Hour(),
+                   dt.Minute(),
+                   dt.Second());
+        Serial.print(datestring);
+    }
 }
 
 void setup()
 {
-#if DEBUG
-    Serial.begin(57600);
-    Serial.print("compiled: ");
-    Serial.print(__DATE__);
-    Serial.println(__TIME__);
-#endif
+    if (DEBUG_MODE) {
+        Serial.begin(115200);
+        Serial.print("compiled: ");
+        Serial.print(__DATE__);
+        Serial.println(__TIME__);
+    }
 
     // Set pin relai sebagai output
     pinMode(RELAY1_PIN, OUTPUT);
@@ -54,54 +56,46 @@ void setup()
 
     RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
     printDateTime(compiled);
-#if DEBUG
-    Serial.println();
-#endif
+    
+    if (DEBUG_MODE) {
+        Serial.println();
+    }
 
     if (!Rtc.IsDateTimeValid())
     {
-#if DEBUG
-        Serial.println("RTC lost confidence in the DateTime!");
-#endif
+        if (DEBUG_MODE) Serial.println("RTC lost confidence in the DateTime!");
         Rtc.SetDateTime(compiled);
     }
 
     if (Rtc.GetIsWriteProtected())
     {
-#if DEBUG
-        Serial.println("RTC was write protected, enabling writing now");
-#endif
+        if (DEBUG_MODE) Serial.println("RTC was write protected, enabling writing now");
         Rtc.SetIsWriteProtected(false);
     }
 
     if (!Rtc.GetIsRunning())
     {
-#if DEBUG
-        Serial.println("RTC was not actively running, starting now");
-#endif
+        if (DEBUG_MODE) Serial.println("RTC was not actively running, starting now");
         Rtc.SetIsRunning(true);
     }
 
     RtcDateTime now = Rtc.GetDateTime();
     if (now < compiled)
     {
-#if DEBUG
-        Serial.println("RTC is older than compile time!  (Updating DateTime)");
-#endif
+        if (DEBUG_MODE) Serial.println("RTC is older than compile time!  (Updating DateTime)");
         Rtc.SetDateTime(compiled);
     }
     else if (now > compiled)
     {
-#if DEBUG
-        Serial.println("RTC is newer than compile time. (this is expected, but still we need to update, if necessary)");
-#endif
-        // Rtc.SetDateTime(compiled);
+        if (DEBUG_MODE) Serial.println("RTC is newer than compile time. (this is expected, but still we need to update, if necessary (the drift is too far))");
+
+        if (FIX_TIME) {
+            Rtc.SetDateTime(compiled);
+        }
     }
     else if (now == compiled)
     {
-#if DEBUG
-        Serial.println("RTC is the same as compile time! (not expected but all is fine)");
-#endif
+        if (DEBUG_MODE) Serial.println("RTC is the same as compile time! (not expected but all is fine)");
     }
 }
 
@@ -109,16 +103,40 @@ void loop()
 {
     RtcDateTime now = Rtc.GetDateTime();
 
+    // ==============================================================
+    // FITUR KOMPENSASI DRIFT OTOMATIS (Mundur 20 detik setiap hari)
+    // ==============================================================
+    static uint8_t lastDay = 99; // Variabel statis untuk melacak perubahan hari
+    
+    // Jika hari berganti (melewati tengah malam)
+    if (now.Day() != lastDay && lastDay != 99) {
+        int driftCorrectionSeconds = -20; 
+        
+        // Ambil waktu saat ini dalam format Unix Epoch, tambahkan koreksi (-20 detik)
+        uint32_t correctedEpoch = now.Unix32Time() + driftCorrectionSeconds;
+        
+        // Simpan waktu yang sudah dikoreksi kembali ke modul RTC
+        Rtc.SetDateTime(RtcDateTime(correctedEpoch));
+        
+        // Perbarui variabel 'now' dengan waktu yang baru dikoreksi
+        now = Rtc.GetDateTime(); 
+        
+        if (DEBUG_MODE) {
+            Serial.println("Kompensasi waktu harian diterapkan: -20 detik");
+        }
+    }
+    lastDay = now.Day(); // Simpan tanggal hari ini untuk pengecekan besok
+    // ==============================================================
+
     printDateTime(now);
-#if DEBUG
-    Serial.println();
-#endif
+    
+    if (DEBUG_MODE) {
+        Serial.println();
+    }
 
     if (!now.IsValid())
     {
-#if DEBUG
-        Serial.println("RTC lost confidence in the DateTime!");
-#endif
+        if (DEBUG_MODE) Serial.println("RTC lost confidence in the DateTime!");
     }
 
     // Atur jadwal nyala/mati lampu
@@ -137,9 +155,7 @@ void loop()
         {
             lampuNyala = true;
         }
-#if DEBUG
-        Serial.println("Time period does not span midnight");
-#endif
+        if (DEBUG_MODE) Serial.println("Time period does not span midnight");
     }
     else
     {
@@ -149,31 +165,25 @@ void loop()
         {
             lampuNyala = true;
         }
-#if DEBUG
-        Serial.println("Time period spans midnight");
-#endif
+        if (DEBUG_MODE) Serial.println("Time period spans midnight");
     }
 
-#if DEBUG
-    Serial.print("lampuNyala: ");
-    Serial.println(lampuNyala);
-#endif
+    if (DEBUG_MODE) {
+        Serial.print("lampuNyala: ");
+        Serial.println(lampuNyala);
+    }
 
     if (lampuNyala)
     {
         digitalWrite(RELAY1_PIN, HIGH); // Nyalakan lampu 1
         digitalWrite(RELAY2_PIN, HIGH); // Nyalakan lampu 2
-#if DEBUG
-        Serial.println("Lampu nyala");
-#endif
+        if (DEBUG_MODE) Serial.println("Lampu nyala");
     }
     else
     {
         digitalWrite(RELAY1_PIN, LOW); // Matikan lampu 1
         digitalWrite(RELAY2_PIN, LOW); // Matikan lampu 2
-#if DEBUG
-        Serial.println("Lampu mati");
-#endif
+        if (DEBUG_MODE) Serial.println("Lampu mati");
     }
 
     delay(10000); // Delay 10 detik
